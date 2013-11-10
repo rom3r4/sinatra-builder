643a8ba893aad6e907e39a4d89de33729640a8cc
Julian Romera Juarez <julian.romera@gmail.com>
2013-11-10 07:53:33 +0100

    license. added script. doc

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ### sinatra-builder
 
 A lightweight Sinatra skeleton build script
+
+### Install
+
+    curl -s https://raw.github.com/julianromerajuarez/sinatra-builder/master/build.sh | sh
+
+### 'License'
+
+This is free and unemcumbered software released into the public domain. For more information, see the accompanying UNLICENSE file.
+
+If you're unfamiliar with public domain, that means it's perfectly fine to start with this skeleton and code away, later relicensing as you see fit.
--- build.sh
+++ build.sh
@@ -5,3 +5,64 @@ if [ "x${1}" = "x" ];then
   exit 1
 fi
 
+if [ -f "$1" || -d "$1" ];then
+  echo "${1} already exists"
+  exit 1
+fi
+
+echo "Creating directories.."
+mkdir -p ${1} ${1}/tmp ${1}/views ${1}/public ${1}/public/javascript ${1}/public/css ${1}/public/images
+
+echo "Creating files.."
+touch ${1}/app.rb ${1}/config.ru ${1}/Gemfile
+
+
+
+cat <<EOT >${1}/Gemfile
+# Gemfile
+source :rubygems
+ 
+ gem "sinatra"
+ gem "haml"
+EOT
+
+
+cat <<EOT >${1}/config.ru
+# config.ru
+require "rubygems"
+require "bundler/setup"
+require "sinatra"
+require "haml"
+require "app"
+ 
+ set :run, false
+ set :raise_errors, true
+  
+  run Sinatra::Application
+EOT
+
+
+
+cat <<EOT >${1}/app.rb
+# app.rb
+set :haml, :format => :html5
+ 
+ get "/" do
+   haml :index
+   end
+EOT
+
+
+cat <<EOT >${1}/views/index.haml
+# views/index.haml
+%html
+  %head
+    %title My title
+      %body
+
+
+echo "Creating bundle.."                 
+cd $1
+rvm all do bundle install
+
+echo "run 'rackup' to start your app"
